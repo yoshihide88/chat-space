91b067b40e395c83ba7f161f99f7b43507da8119
yoshihide88 <mini8686mini0112@gmail.com>
2019-09-05 16:47:04 -0700

    mm

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,12 +7,10 @@ worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
 
+working_directory app_path
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
 #ポート番号を指定
-listen 3000
-
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
