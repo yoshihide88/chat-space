69a89ce7b853e54b88291b74cbb7cfd5a5ecedc4
yoshihide88 <mini8686mini0112@gmail.com>
2019-09-07 14:48:38 +0900

    kk

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,21 +5,21 @@ $(function() {
       //data-idが反映されるようにしている
       var addImage = message.image? `<img src=${message.image} class="lower-message__image" > ` : ""; 
   
-      var html = `<div class="message" data-id=${ message.id }>
-        div class="upper-message"> 
-          <div class="upper-message__user-name">
-            ${message.username }
-          </div>
-          <div class="upper-message__date">
+      var html = `<div class="contents__right" data-id=${ message.id }>
+        <div class="contents__right__text"> 
+          <li>
+            ${message.user_name }
+          </li>
+          <li class="fff">
            ${ message.created_at }
-          </div>
-          </div>
-          <div class="lower-message">
+          </li>
+        </div>
+          <div class="contents__right__text-details">
             <p class="lower-message__content">
             ${message.content }
             </p>
           ${addImage}
-        </div>
+          </div>
        </div>`
    
     return html;
@@ -27,7 +27,7 @@ $(function() {
     var reloadMessages = function() {
       if (window.location.href.match(/groups\/\d+\/messages/)){
       //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      last_message_id = $(".contents__right").last().attr("data-id");
+      last_message_id = $(".contents__right:last").data("id");
       $.ajax({
         //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: "api/messages",
@@ -42,8 +42,8 @@ $(function() {
         messages.forEach(function(message){
           insertHTML = buildMessageHTML(message);
           $(".contents-messages").append(insertHTML);
+          $(".contents-messages").animate({scrollTop: $(".contents-messages")[0].scrollHeight},500);
         })
-        $(".contents-messages").animate({scrollTop: $(".contents-messages")[0].scrollHeight},500);
       })
       .fail(function() {
         alert('error');
--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,13 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-   config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.test_framework false
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
-end
 end
