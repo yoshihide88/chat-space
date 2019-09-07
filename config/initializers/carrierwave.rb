b8e890c23086927b5b13f024a8eeae9e27109502
yoshihide88 <mini8686mini0112@gmail.com>
2019-09-06 17:25:02 -0700

    co

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'mikan88'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mikan88'
+  config.fog_directory  = 'mikan44'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mikan44'
 end
